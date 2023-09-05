970740bb880efd924c19b1c0b01230935289d890
Haseeb0089 <muhammadhaseebshariff@gmail.com>
2023-09-05 12:34:13 +0500

    updated

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,19 @@
 import express from 'express';
 
-import  {newOrder, getSingleOrder} from '../controllers/orderController.js';
+import  {newOrder, getSingleOrder, myOrders , getAllOrders ,updateOrder ,deleteOrder} from '../controllers/orderController.js';
 
 const router = express.Router();
 
 router.post('/new', newOrder);
 router.get('/getSingleOrder', getSingleOrder);
+router.get('/myOrders', myOrders);
+router.get('/getAll', getAllOrders);
+router.put('/updateOrder', updateOrder);
+router.delete('/deleteOrder', deleteOrder);
+
+
+
+
 
 
 
